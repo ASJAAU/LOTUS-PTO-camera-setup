63366c24387a4ed3fcd193ec15ee5a12233470ba
ASJAAU <asjo@create.aau.dk>
2026-03-13 11:49:05 +0100

    Added retry and logging

--- systemd/lotus-capture.sh
+++ systemd/lotus-capture.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-cd /home/aau/LOTUS-PTO_studentwork/
 
+# Setup log dir
 LOGDIR="./logs"
 mkdir -p "$LOGDIR"
 LOGFILE="$LOGDIR/$(date +%Y-%m-%d).log"
 
+echo "ROUTINE: Started routine capture service at: $(date '+%Y-%m-%d-%H-%M-%S')"
+
+# Setup working environment
+cd /home/aau/LOTUS-PTO_studentwork/
 source venv/bin/activate
 
+# Run commands
 ./venv/bin/python3 snap_pic.py >> "$LOGFILE" 2>&1
