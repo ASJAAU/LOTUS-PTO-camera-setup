7efdaab2b3f0c929fc36809feac1c4e5e729fc39
ASJAAU <asjo@create.aau.dk>
2026-03-13 15:56:43 +0100

    Reverted back from relative paths

--- systemd/lotus-capture.sh
+++ systemd/lotus-capture.sh
@@ -8,6 +8,7 @@ LOGFILE="$LOGDIR/$(date +%Y-%m-%d).log"
 echo "ROUTINE: Started routine capture service at: $(date '+%Y-%m-%d-%H-%M-%S')"
 
 # Setup working environment
+cd "/home/aau/LOTUS-PTO_studentwork/"
 source venv/bin/activate
 
 # Run commands
