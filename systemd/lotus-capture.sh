53485cbe04320e0d72da523ba3655e052ce1658f
ASJAAU <asjo@create.aau.dk>
2026-03-13 11:36:12 +0100

    Updated bash script to write daily log files

--- systemd/lotus-capture.sh
+++ systemd/lotus-capture.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 cd /home/aau/LOTUS-PTO_studentwork/
 
+LOGDIR="./logs"
+mkdir -p "$LOGDIR"
+LOGFILE="$LOGDIR/$(date +%Y-%m-%d).log"
+
 source venv/bin/activate
 
-./venv/bin/python3 capture.py
+./venv/bin/python3 snap_pic.py >> "$LOGFILE" 2>&1
