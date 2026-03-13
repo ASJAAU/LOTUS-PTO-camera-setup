f483dbfb617abd901e2ff4f3eb969d314e299582
ASJAAU <asjo@create.aau.dk>
2026-03-13 13:17:48 +0100

    Set working directory at service scope

--- systemd/lotus-capture.sh
+++ systemd/lotus-capture.sh
@@ -8,7 +8,6 @@ LOGFILE="$LOGDIR/$(date +%Y-%m-%d).log"
 echo "ROUTINE: Started routine capture service at: $(date '+%Y-%m-%d-%H-%M-%S')"
 
 # Setup working environment
-cd /home/aau/LOTUS-PTO_studentwork/
 source venv/bin/activate
 
 # Run commands
